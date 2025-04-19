248784ac007390d075291f1177b5783f9ace71a7
Tushar Sharma <sharma.tushar.271003@gmail.com>
2025-04-19 19:32:40 +0530

    added cors

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/tushar27x/portfolioV2-backend/config"
 	"github.com/tushar27x/portfolioV2-backend/migrations"
@@ -24,6 +26,16 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://your-production-domain.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.RegisterAllRoutes(r, db)
 
 	r.Run(":" + port)
